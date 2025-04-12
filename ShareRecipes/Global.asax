9338c9521eb38f6f99fce46a2402a918abea942f
thaih <thaih@DESKTOP-LTQ7I3G>
2025-04-12 10:59:08 +0700

    Add project files.

---
+++ ShareRecipes/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShareRecipes.MvcApplication" Language="C#" %>
---
+++ ShareRecipes/Models/MyDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using Models;
+public class MyDbContext : DbContext
+{
+    public MyDbContext() : base("DefaultConnection")
+    {
+    }
+
+    public DbSet<User> Users { get; set; } // Định nghĩa bảng Products
+}
